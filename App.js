8af04c8b973456469ae7a97b8a439e652c14ae45
need0 <136871368+need0@users.noreply.github.com>
2023-08-05 10:23:09 +0700

    axios_library

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ export default function App() {
       {/* <IncrementCounter/> */}
       {/* <Form/> */}
       {/* <Login/> */}
-      <TouchablePractice/>
+      {/* <TouchablePractice/> */}
     </View>
   );
 }
